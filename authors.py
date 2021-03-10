88febf0f8bf5234f4365302c03c0799252f9cee8
Alice <45924289+aliceinnorway@users.noreply.github.com>
2021-03-10 10:53:33 +0100

    Add files via upload

--- authors.py
+++ authors.py
@@ -1,29 +1,32 @@
-
-#Before inserting a string: it must start with a space and end with no spaces or commas.
-old_authors = " "
+#Before inserting a string it must end with no spaces or commas.
+old_authors = ""
 
 # sometimes the authors may be distinguished by & or and instead of , 
 old_authors = old_authors.replace(' &', ',')
 old_authors = old_authors.replace(' and', ',')
+old_authors = old_authors.replace('Kjeld S.', 'Kjeld Steenbjerg') #This is because Wix is only recognising Kjeld as Kjeld Steenbjerg
 
 # splitting all the names
 name_list = old_authors.split(',')
-print(name_list)
+
+# add a space before the first surname
+name_list[0] = ' ' + name_list[0]
+
+# add 'and' for the last name
+name_list[-2] = 'and ' + name_list[-2]
 
 # now to add the names back together
-
 new_names = []
 stop = len(name_list) - 1
 x = range(0, stop, 2)
 
 for i in x:
     first_name = i + 1
+    # this line below is where its switches the names
     full_name = (name_list[first_name] + name_list[i]).lstrip()
     new_names.append(full_name)
     
-
-print(f" These are the new names: {new_names}")
-
 result = ', '.join(str(i) for i in new_names)
 print(result)
-#it could end with 'and ' for the last name...
+
+
